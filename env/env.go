c71a27dd83fc78b8d4d1b2e31896647e8a8fe010
Gema Satya Danera <gema280419@gmail.com>
2026-01-20 14:38:35 +0700

    Change the ReadEnv function

--- env/env.go
+++ env/env.go
@@ -5,36 +5,7 @@ import (
 )
 
 func ReadEnv(req string) string {
-	// f, err := os.Open("D:/Coding/golang/gorm-playground/.env")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// defer f.Close()
-
-	// scanner := bufio.NewScanner(f)
-	// for scanner.Scan() {
-	// 	key := strings.Split(scanner.Text(), "=")
-	// 	if strings.TrimSpace(key[0]) == req {
-	// 		return strings.TrimSpace(key[1])
-	// 	}
-	// }
-
-	// if err := scanner.Err(); err != nil {
-	// 	panic(err)
-	// }
-	
-	// err := godotenv.Load()
-    // if err != nil {
-    //     log.Println("No .env file found, reading from environment variables")
-    // }
-    // // Get port from environment variable
-    // port := os.Getenv("PORT")
-    // if port == "" {
-    //     port = "8080" // default port if not set
-    // }
 
 	return os.Getenv(req)
 
-	// return ""
 }
